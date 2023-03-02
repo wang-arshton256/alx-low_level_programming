2712445ef11fe7f61b22cabba133c06d0032a4aa
arshton <wangarshton@gmail.com>
2023-03-02 14:44:22 -0800

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * Return: two concat strings
+ * Author: Wangutusi Arhston
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
+
+	return (dest);
+}
