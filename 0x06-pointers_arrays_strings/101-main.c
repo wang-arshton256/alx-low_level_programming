efce3ff943fab9e579d6a36bd326f6006afba348
arshton <wangarshton@gmail.com>
2023-03-02 15:04:40 -0800

    edited [F-print_number.c

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -1,27 +1,21 @@
 #include "main.h"
 
 /**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-void print_number(int n)
+int main(void)
 {
-	unsigned int n1;
-
-	n1 = n;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n1 = -n;
-	}
-
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
 }
-
