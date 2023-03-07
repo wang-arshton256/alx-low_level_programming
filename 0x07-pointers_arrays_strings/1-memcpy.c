43efedb8fb533ef6da67e43e3d806dde0fb077c9
arshton <wangarshton@gmail.com>
2023-03-06 16:02:10 -0800

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ * @dest: memory area
+ * @src: source
+ * @n: length of src
+ *
+ * Return: pointer to dest.
+**/
+
+void *_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned int index;
+	unsigned char *destination = dest;
+	const unsigned char *source = src;
+
+	for (index = 0; index < n; index++)
+		destination[index] = source[index];
+
+	return (dest);
+}
