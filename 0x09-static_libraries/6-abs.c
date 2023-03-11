af70a929b64f1a9be7281f6cc14f7833a7f24b88
Wang Arshton C <wangarshton@gmail.com>
2023-03-11 05:32:10 +0300

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -6,10 +6,16 @@
  */
 int _abs(int n)
 {
-if (n >= 0)
-{
-return (n);
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
 }
-return (-n);
-}
-
