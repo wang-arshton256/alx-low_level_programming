b46e0fe47d1c82e6aa5269c976ef1c51574dd7c0
Wang Arshton C <wangarshton@gmail.com>
2023-03-11 05:25:52 +0300

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -11,18 +11,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int j;
-j = 0;
-while (j < n && src[j] != '\0')
-{
-dest[j] = src[j];
-j++;
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
 }
-while (j < n)
-{
-dest[j] = '\0';
-j++;
-}
-return (dest);
-}
+
 
