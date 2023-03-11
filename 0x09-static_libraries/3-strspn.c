465b3b125922e13066f299183490ed5ad7e9bfc8
Wang Arshton C <wangarshton@gmail.com>
2023-03-11 05:29:15 +0300

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -7,22 +7,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int n = 0;
-int r;
-while (*s)
-{
-for (r = 0; accept[r]; r++)
-{
-if (*s == accept[r])
-{
-n++;
-break;
-}
-else if (accept[r + 1] == '\0')
-return (n);
-}
-s++;
-}
-return (n);
-}
+	unsigned int bytes = 0;
+	int index;
 
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+
+		s++;
+	}
+
+	return (bytes);
+}
