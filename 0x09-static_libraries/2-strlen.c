e30e9cd9ffdb6aaf96320674fc4ec2e31e12d535
Wang Arshton C <wangarshton@gmail.com>
2023-03-11 05:24:44 +0300

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -6,12 +6,11 @@
  */
 int _strlen(char *s)
 {
-int longi = 0;
-while (*s != '\0')
-{
-longi++;
-s++;
+	int length = 0;
+
+	while (s[length])
+		length++;
+	return (length);
 }
-return (longi);
-}
+
 
