dbdea5ee46aa7e33ce131eb288f9a63d5e378541
Wang Arshton C <wangarshton@gmail.com>
2023-03-11 05:30:52 +0300

    Update 4-strpbrk.c

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -7,16 +7,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-int k;
-while (*s)
-{
-for (k = 0; accept[k]; k++)
-{
-if (*s == accept[k])
-return (s);
-}
-s++;
-}
-return ('\0');
-}
+	int index;
 
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+
+		s++;
+	}
+	return ('\0');
+}
