064693dc81043a6a5f9e9e82dba850f6f4fcad55
Wang Arshton C <wangarshton@gmail.com>
2023-03-11 05:31:33 +0300

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -7,18 +7,29 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-for (; *haystack != '\0'; haystack++)
-{
-char *l = haystack;
-char *p = needle;
-while (*l == *p && *p != '\0')
-{
-l++;
-p++;
-}
-if (*p == '\0')
-return (haystack);
-}
-return (0);
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
 }
 
