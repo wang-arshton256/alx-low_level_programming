abc562f7ac939cdcfa29273fb8e5ed67e1cbec5c
Wang Arshton C <wangarshton@gmail.com>
2023-03-11 05:32:50 +0300

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -7,17 +7,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int l = 0;
-int x = 0;
-while (*(src + l) != '\0')
-{
-l++;
-}
-for ( ; x < l ; x++)
-{
-dest[x] = src[x];
-}
-dest[l] = '\0';
-return (dest);
-}
+	int i = -1;
 
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
