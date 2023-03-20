03e196c6be64cd9536fad3e1db4d2c3c4c2cc582
arshton <wangarshton@gmail.com>
2023-03-20 16:24:15 -0700

    0x0E. C - Structures, typedef

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -1,28 +1,35 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
+
 
 /**
- * dog_t - Typedef for struct dog
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
  */
-typedef struct dog dog_t;
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+
 
 /**
- * struct dog - struct that stores dog information
- * @name: dog name
- * @age: age of the dog
- * @owner: owner of the dog
- * Description: struct called "dog" that stores its info
+ * dog_t - typedef for struct dog
  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+	typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+	char *_strcpy(char *dest, char *src);
+	int _strlen(char *s);
 
 #endif
+
