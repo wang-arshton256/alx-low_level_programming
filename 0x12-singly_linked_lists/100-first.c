cb9177e86206254b00f8ba47094f12688674cb6b
arshton <wangarshton@gmail.com>
2023-03-30 15:32:53 -0700

    fixed task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,7 +10,7 @@ void myStartupFun(void) __attribute__((constructor));
 
 /**
 * myStartupFun - implementation of myStartupFun
-*/
+**/
 void myStartupFun(void)
 {
    printf("You're beat! and yet, you must allow,\n");
