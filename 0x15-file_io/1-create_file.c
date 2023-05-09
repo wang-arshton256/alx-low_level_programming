752085a89ff8b6ecd3689301d9055a9b827f29ee
wang-arshton256 <wangarshton@gmail.com>
2023-05-08 17:53:45 -0700

    A function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int k, ryt, len = 0;
+	int jos, ryt, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,14 +19,14 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-    
-    ryt = write(k, text_content, len);
-	k = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	if (k == -1 || ryt == -1)
+	jos = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	ryt = write(jos, text_content, len);
+
+	if (jos == -1 || ryt == -1)
 		return (-1);
 
-	close(k);
+	close(jos);
 
 	return (1);
 }
