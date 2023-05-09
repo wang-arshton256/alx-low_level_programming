4ea51bea600586c3596f8d8fc6c8d8a5e8ee0cfb
wang-arshton256 <wangarshton@gmail.com>
2023-05-09 13:33:45 -0700

    A program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,15 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * Description : Header file containing prototypes for all functions
+ *       written in the 0x15-file_io directory.
+ */
 
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <sys/types.h>
+#include <unistd.h>
 
-
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif 
+/* MAIN_H */
