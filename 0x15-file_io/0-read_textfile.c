c8892e5f47afefc51290eb9da39e0fcac0272e03
wang-arshton256 <wangarshton@gmail.com>
2023-05-08 17:24:36 -0700

    A function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ *
+ *read_textfile: Reads the text file print to STDOUT.
+ *@letters: These are the number of letters to be read by the function
+ *@filename: This is the text file being read
+ *Returns: w- the number of bytes read and printed
+ *0 when function fails or when the filename is Null.
+ */
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+    	ssize_t ryt;
+	ssize_t x;
+	ssize_t j;
+
+	x = open(filename, O_RDONLY);
+	if (x == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	j = read(x, buffer, letters);
+	ryt = write(STDOUT_FILENO, buffer, j);
+
+	free(buffer);
+	close(x);
+	return (ryt);
+}
+
+
