f7c2b6b0b2e4ca9f324b16cd939460377fa2da2b
wang-arshton256 <wangarshton@gmail.com>
2023-07-06 18:02:31 -0700

    3-set_bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit: Sets the value of a bit to 1.
+ * at a given index.
+ * @n: Pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if there is an error.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int k;
+
+	if (index > 63)
+		return (-1);
+
+	k = 1 << index;
+	*n = (*n | k);
+	return (1);
+}
