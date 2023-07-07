b05e5875685d5bc257e853466c8f250fdbef1043
wang-arshton256 <wangarshton@gmail.com>
2023-07-06 17:15:01 -0700

    0-binary_to_unit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary-to-uint : COnverts a binary number to unsigned int
+ * @b: String Containing The binary number
+ *
+ * Return: The converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int w;
+	unsigned int deci_val = 0;
+
+	if (!b)
+		return (0);
+
+	for (w = 0; b[w]; w++)
+	{
+		if (b[w] < '0' || b[w] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[w] - '0');
+	}
+
+	return (deci_val);
+}
+
